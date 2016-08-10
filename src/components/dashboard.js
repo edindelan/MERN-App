8f2326bb8fa205914016ef6009716e72c097ca9d
Edin Delanovic <edin.delanovic@smava.de>
2016-08-10 17:19:57 +0200

    Routes for Login and Dashboard

---
+++ src/components/dashboard.js
@@ -0,0 +1,12 @@
+/**
+ * Created by edelanovic on 10.08.16.
+ */
+import React, {Component} from 'react';
+
+export default class Dashboard extends Component{
+    render() {
+        return <div>
+            This is dashboard!
+        </div>
+    }
+}
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+/**
+ * Created by edelanovic on 10.08.16.
+ */
+import React, {Component} from 'react';
+
+export default class Login extends Component {
+    render() {
+        return <div>
+            This is login!
+        </div>
+    }
+}
